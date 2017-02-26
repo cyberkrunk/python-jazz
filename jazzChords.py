765b77dfadcfa72aa2e17c222406f1fc11dde766
Chester Jankowski <chester.jankowski@gmail.com>
2017-02-25 20:54:59 -0500

    First commit

---
+++ jazzChords.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# This code contains chords commonly used in jazz
+
+# Section 1: 15 4-note 7th chords in root position, close position
+
+M7 = [0, 4, 7, 11]
+M7b5 = [0, 4, 6, 11]
+M7s5 = [0, 4, 8, 11]
+M6 = [0, 4, 7, 9]
+m7 = [0, 3, 7, 10]
+m7b5 = [0, 3, 6, 10]
+m7s5 = [0, 3, 8, 10]
+m6 = [0, 3, 7, 9]
+mM7 = [0, 3, 7, 11]
+D7 = [0, 4, 7, 10]
+D7b5 = [0, 4, 6, 10]
+D7s5 = [0, 4, 8, 10]
+D7sus4 = [0, 5, 7, 10]
+dim7 = [0, 3, 6, 9]
+dimM7 = [0, 3, 6, 11]
+
+print (M7)
+
+print (M7[1])
+
