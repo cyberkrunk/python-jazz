7487da8bff75a389d1e1a8fc4d827c826f810a4c
Chester Jankowski <chester.jankowski@gmail.com>
2017-02-26 01:01:18 -0500

    .

---
+++ scratch.py
@@ -0,0 +1,23 @@
+exp2 = []
+exp3 = []
+exp5 = []
+
+exp2 = [2**x for x in range(17)]
+exp3 = [3**y for y in range(13)]
+exp5 = [5**z for z in range(13)]
+
+print(exp2)
+
+print()
+
+print(exp3)
+
+print()
+
+print(exp5)
+
+print()
+
+Blerg = list(range(100))
+print(Blerg)
+
